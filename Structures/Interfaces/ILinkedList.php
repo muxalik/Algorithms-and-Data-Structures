ea44ebc4891a020aa2b23a5df80af7cb5bcbeec2
muxailk <muxail1327@mail.ru>
2023-01-09 11:36:29 +0300

    Add SinglyLinkedList

---
+++ Structures/Interfaces/ILinkedList.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Structures\Interfaces;
+
+use Structures\Support\LinkedListNode;
+
+interface ILinkedList
+{
+    public function addFirst(mixed $value): int;
+    public function addLast(mixed $value): int;
+    public function set(int $position, mixed $value): void;
+    
+    public function get(int $position): ?LinkedListNode;
+    public function getFirst(): ?LinkedListNode;
+    public function getLast(): ?LinkedListNode;
+
+    public function remove(int $position): void;
+    public function clear(): void;
+}
